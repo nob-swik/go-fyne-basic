2fb41b884f50601fb591a027bc90c19ee27dc51d
nob-swik <nobusuke.sawaike@denso-ten.com>
2023-05-19 17:57:28 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+
+	w := a.NewWindow("Hello")
+	w.SetContent(
+		widget.NewVBox(
+			widget.NewLabel("Hello Fyne!"),
+			widget.NewLabel("This is sample application!"),
+		),
+	)
+
+	w.ShowAndRun()
+
+}
