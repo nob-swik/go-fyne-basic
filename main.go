a0e54d55b6424da30015fab3f77d4325209a8aa5
nob-swik <nobusuke.sawaike@denso-ten.com>
2023-05-26 07:46:11 +0900

    use ToolBar and MainMenu

--- main.go
+++ main.go
@@ -3,25 +3,44 @@ package main
 import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/theme"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
+	l := widget.NewLabel("Hello Fyne!")
+	tb := widget.NewToolbar(
+		widget.NewToolbarAction(theme.HomeIcon(), func() {
+			l.SetText("Select Home Icon!")
+		}),
+		widget.NewToolbarAction(theme.InfoIcon(), func() {
+			l.SetText("Select Information Icon!")
+		}),
+	)
+	mm := fyne.NewMainMenu(
+		fyne.NewMenu("File",
+			fyne.NewMenuItem("New", func() {
+				l.SetText("select New menu item.")
+			}),
+			fyne.NewMenuItem("Quit", func() {
+				a.Quit()
+			}),
+		),
+	)
+	w.SetMainMenu(mm)
 	w.SetContent(
 		fyne.NewContainerWithLayout(
-			layout.NewGridLayout(3),
-			widget.NewButton("one", nil),
-			widget.NewButton("two", nil),
-			widget.NewButton("three", nil),
-			widget.NewButton("four", nil),
-			layout.NewSpacer(),
-			widget.NewButton("five", nil),
+			layout.NewBorderLayout(
+				nil, tb, nil, nil,
+			),
+			l,
+			tb,
 		),
 	)
 	a.Settings().SetTheme(theme.LightTheme())
+	w.Resize(fyne.NewSize(800, 600))
 	w.ShowAndRun()
 }
