3517fced5f55aa46974446be7ce51f650ae4fbf8
nob-swik <nobusuke.sawaike@denso-ten.com>
2023-06-02 18:12:51 +0900

    Create new, save, open, and quit functions for text editor GUI app

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
@@ -31,44 +36,104 @@ func (e *MyEntry) KeyDown(key *fyne.KeyEvent) {
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
-	l := widget.NewLabel("Hello Fyne!")
-	tb := widget.NewToolbar(
-		widget.NewToolbarAction(theme.HomeIcon(), func() {
-			l.SetText("Select Home Icon!")
-		}),
-		widget.NewToolbarAction(theme.InfoIcon(), func() {
-			l.SetText("Select Information Icon!")
-		}),
-	)
+	a.Settings().SetTheme(theme.LightTheme())
+	w := a.NewWindow("Text Editor by Fyne")
+	sta := widget.NewLabel("status.")
+
+	edit := widget.NewEntry()
+	edit.MultiLine = true
+	sc := container.NewScroll(edit)
+
+	// new file function
+	nf := func() {
+		dialog.ShowConfirm("Alert", "Create new document?", func(b bool) {
+			if b {
+				edit.SetText("")
+				sta.SetText("create new file.")
+			}
+		}, w)
+	}
+
+	// save file function
+	sf := func() {
+		sd := dialog.NewFileSave(func(uc fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			if uc == nil {
+				return
+			}
+			fmt.Println(uc.URI().String())
+			err = os.WriteFile(uc.URI().String()[7:], []byte(edit.Text), os.ModePerm)
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			sta.SetText("Save file to '" + uc.URI().String()[7:] + "'.")
+		}, w)
+		sd.Show()
+	}
+
+	// open file function
+	of := func() {
+		od := dialog.NewFileOpen(func(uc fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			if uc == nil {
+				return
+			}
+			cont, err := os.ReadFile(uc.URI().String()[7:])
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			edit.SetText(string(cont))
+			sta.SetText("Open file from '" + uc.URI().String()[7:] + "'.")
+		}, w)
+		od.Show()
+	}
+
 	mm := fyne.NewMainMenu(
 		fyne.NewMenu("File",
 			fyne.NewMenuItem("New", func() {
-				l.SetText("select New menu item.")
+				nf()
+			}),
+			fyne.NewMenuItem("Open...", func() {
+				of()
+			}),
+			fyne.NewMenuItem("Save...", func() {
+				sf()
 			}),
 			fyne.NewMenuItem("Quit", func() {
 				a.Quit()
 			}),
 		),
+		fyne.NewMenu("Edit",
+			fyne.NewMenuItem("Cut", func() {
+			}),
+			fyne.NewMenuItem("Copy", func() {
+			}),
+			fyne.NewMenuItem("Paste", func() {
+			}),
+			fyne.NewMenuItem("Change Theme", func() {
+			}),
+		),
 	)
 	w.SetMainMenu(mm)
-	e := NewMyEntry(func(e *MyEntry) {
-		s := e.Text
-		e.SetText("")
-		l.SetText("you type '" + s + "'.")
-	})
+
 	w.SetContent(
 		fyne.NewContainerWithLayout(
 			layout.NewBorderLayout(
-				nil, tb, nil, nil,
+				nil, sta, nil, nil,
 			),
-			widget.NewVBox(
-				l,e,
-			),
-			tb,
+			sc,
+			sta,
 		),
 	)
-	a.Settings().SetTheme(theme.LightTheme())
+
 	w.Resize(fyne.NewSize(800, 600))
 	w.ShowAndRun()
 }
